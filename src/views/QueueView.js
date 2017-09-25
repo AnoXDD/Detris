f99e6c6ade177b6058c72e5c8ef4908d2d670338
Anoxic <guanrunjie@gmail.com>
2017-09-25 17:10:38 -0400

    Add text box around queue

--- src/views/QueueView.js
+++ src/views/QueueView.js
@@ -6,6 +6,7 @@ import React, {Component} from "react";
 import {CSSTransitionGroup} from 'react-transition-group';
 
 import DetrominoView from "./DetrominoView";
+import TitleBoxView from "./TitleBoxView";
 
 export default class QueueView extends Component {
 
@@ -22,17 +23,44 @@ export default class QueueView extends Component {
 
     return (
       <div className="queue-view">
-        <CSSTransitionGroup
-          transitionName="detromino-animation"
-          transitionEnterTimeout={200}
-          transitionLeaveTimeout={200}
-        >
-          {this.props.queue.reverse().map((detromino, i) =>
-            <div key={size - i} className="detromino-view-wrapper">
-              <DetrominoView type={detromino}/>
+        <div className="next">
+          <TitleBoxView title="next">
+            <div className="next-inner">
+              <CSSTransitionGroup
+                transitionName="detromino-animation"
+                transitionEnterTimeout={200}
+                transitionLeaveTimeout={200}
+              >
+                <div key={new Date().getTime()}
+                     className="detromino-view-wrapper">
+                  <DetrominoView type={this.props.queue.last()}/>
+                </div>
+              </CSSTransitionGroup>
             </div>
-          )}
-        </CSSTransitionGroup>
+          </TitleBoxView>
+        </div>
+        <div className="queue">
+          <div className="flex-inner-extend">
+            <div className="queue-flex-inner-extend-wrapper">
+              <TitleBoxView title="queue">
+                <div className="queue-inner">
+                  <CSSTransitionGroup
+                    transitionName="detromino-animation"
+                    transitionEnterTimeout={200}
+                    transitionLeaveTimeout={200}
+                  >
+                    {this.props.queue.reverse().map((detromino, i) =>
+                      i > 0 ?
+                        <div key={size - i} className="detromino-view-wrapper">
+                          <DetrominoView type={detromino}/>
+                        </div> : null
+                    )}
+                  </CSSTransitionGroup>
+                </div>
+              </TitleBoxView>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
