7ddbdfdfce958600af320cecf1a7f25b4f2ced5e
Anoxic <guanrunjie@gmail.com>
2018-01-12 16:29:39 -0500

    Adding middleware to store.js

--- src/data/store.js
+++ src/data/store.js
@@ -2,10 +2,11 @@
  * Created by Anoxic on 1/12/2018.
  */
 
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 
 import reducer from "../reducer/reducer";
+import delayDispatcher from "../middleware/delayDispatcher";
 
-const store = createStore(reducer);
+const store = createStore(reducer, undefined, applyMiddleware(delayDispatcher));
 
 export default store;
