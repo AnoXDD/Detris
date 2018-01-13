a41cfbd6824d4fe4b33d76029be2bc59dc65542c
Anoxic <guanrunjie@gmail.com>
2018-01-12 23:48:19 -0500

    Connect to Chrome Devtool

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,16 @@
  * Created by Anoxic on 1/12/2018.
  */
 
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 
 import reducer from "../reducer/reducer";
 import {delayDispatch} from "../middleware/delayDispatcher";
 
-const store = createStore(reducer, undefined, applyMiddleware(delayDispatch));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(reducer,
+  undefined,
+  composeEnhancers(applyMiddleware(delayDispatch)));
 
 export default store;
