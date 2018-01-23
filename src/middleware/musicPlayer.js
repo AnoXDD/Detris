0fa6d69992ef4a88b1cfde073e1a0374011e4fc3
Anoxic <guanrunjie@gmail.com>
2018-01-23 14:12:46 -0500

    Adding sound to some actions

--- src/middleware/musicPlayer.js
+++ src/middleware/musicPlayer.js
@@ -23,6 +23,49 @@ export default function musicPlayer(store) {
         musicType = MusicType.DIALOG_CLOSE;
         break;
 
+      case ActionType.DISPLAY_SUCCESS:
+        musicType = MusicType.NOTIFICATION_SUCCESS;
+        break;
+
+      case ActionType.LEVEL_FAIL:
+      case ActionType.DISPLAY_ERROR:
+        musicType = MusicType.NOTIFICATION_ERROR;
+        break;
+
+      case ActionType.SET_GAME_UI_STATE:
+      case ActionType.START_LEVEL:
+      case ActionType.LEVEL_NEXT_PAGE:
+      case ActionType.LEVEL_PREV_PAGE:
+        musicType = MusicType.BUTTON_CLICK;
+        break;
+
+      case ActionType.LEVEL_SUCCESS:
+        musicType = MusicType.LEVEL_SUCCESS;
+        break;
+
+      case ActionType.ROTATE:
+      case ActionType.DETROMINO_MOVE_LEFT:
+      case ActionType.DETROMINO_MOVE_RIGHT:
+      case ActionType.DETROMINO_MOVE_UP:
+      case ActionType.DETROMINO_MOVE_DOWN:
+      case ActionType.ENABLE_BLOCK_EDITING:
+      case ActionType.DISABLE_BLOCK_EDITING:
+      case ActionType.EDITOR_DETROMINO_MOVE_LEFT:
+      case ActionType.EDITOR_DETROMINO_MOVE_RIGHT:
+      case ActionType.EDITOR_DETROMINO_MOVE_UP:
+      case ActionType.EDITOR_DETROMINO_MOVE_DOWN:
+      case ActionType.EDITOR_BLOCK_MOVE_LEFT:
+      case ActionType.EDITOR_BLOCK_MOVE_RIGHT:
+      case ActionType.EDITOR_BLOCK_MOVE_UP:
+      case ActionType.EDITOR_BLOCK_MOVE_DOWN:
+      case ActionType.SET_BLOCKTYPE:
+      case ActionType.NEXT_DETROMINO_SHAPE:
+      case ActionType.PREV_DETROMINO_SHAPE:
+      case ActionType.UNDO_IN_EDITOR:
+      case ActionType.REDO_IN_EDITOR:
+        musicType = MusicType.CONTROL_CLICK;
+        break;
+
       default:
     }
 
