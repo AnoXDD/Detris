3f84d9702aa8af39a581036d31c0ee3db8055c77
Anoxic <guanrunjie@gmail.com>
2018-01-23 20:12:30 -0500

    Changed what credit looks like

--- src/components/fullscreenOverlay/AboutView.js
+++ src/components/fullscreenOverlay/AboutView.js
@@ -24,14 +24,15 @@ export default class AboutView extends Component {
           <p className="title">Developer</p>
           <p className="sub-title">Design & Programming</p>
           <p>Runjie Guan</p>
-          <a href="https://anoxic.me">https://anoxic.me</a>
+          <p><a href="https://anoxic.me">https://anoxic.me</a></p>
         </div>
         <div className="about-section">
           <p className="title">Open source</p>
-          Original game <a href="https://github.com/AnoXDD/Detris">on Github</a>
+          <p>Original game <a href="https://github.com/AnoXDD/Detris">on
+            Github</a></p>
           <p className="sub-title">Icons</p>
-          <a href="https://github.com/google/material-design-icons/">Google
-            Material Design Icons</a>
+          <p><a href="https://github.com/google/material-design-icons/">Google
+            Material Design Icons</a></p>
         </div>
       </FullscreenOverlayView>
     );
